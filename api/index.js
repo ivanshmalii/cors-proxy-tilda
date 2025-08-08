7acf69e3dca29edee3beb02e90684d0f4dc3fac3
ivanshmalii <serialunit.corp@gmail.com>
2025-08-08 02:12:26 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
-export default async function handler(req, res) {
-  const targetUrl = req.query.url;
-  if (!targetUrl) {
-    return res.status(400).send('Missing "url" query parameter.');
+module.exports = async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    res.status(400).send('Missing "url" query parameter.');
+    return;
   }
 
   try {
-    const response = await fetch(targetUrl);
+    const response = await fetch(url);
     const data = await response.arrayBuffer();
 
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,5 +16,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).send('Error fetching the target URL.');
   }
-}
-
+};
